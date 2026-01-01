0644850c8659bcbeb7b637c9fba7cf5c5002e0c0
shiwali-thakur <thakurshiwali8@gmail.com>
2026-01-01 18:24:44 +1100

    added adsense code and updated pages

--- app.js
+++ app.js
@@ -1,30 +1,25 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
 const app = express();
+const path = require('path');
 
-// Set view engine
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Serve static files
-app.use(express.static(path.join(__dirname, "public")));
+// Routes
+app.get('/', (req, res) => res.render('home'));
+app.get('/articles', (req, res) => res.render('articles'));
 
-// Home route
-app.get("/", (req, res) => {
-    res.render("home");
-});
+// Article Pages
+app.get('/post', (req, res) => res.render('post'));
+app.get('/post1', (req, res) => res.render('post1'));
+app.get('/post2', (req, res) => res.render('post2'));
+app.get('/post3', (req, res) => res.render('post3'));
+app.get('/post4', (req, res) => res.render('post4'));
+app.get('/post4', (req, res) => res.render('post5'));
+// Other Pages
+app.get('/about', (req, res) => res.render('about'));
+app.get('/privacy', (req, res) => res.render('privacy'));
+app.get('/contact', (req, res) => res.render('contact'));
 
-// Sample PTE article pages
-app.get("/post", (req, res) => res.render("post"));
-app.get("/post1", (req, res) => res.render("post1"));
-app.get("/post2", (req, res) => res.render("post2"));
-app.get("/post3", (req, res) => res.render("post3"));
-app.get("/post4", (req, res) => res.render("post4"));
-app.get("/post5", (req, res) => res.render("post5"));
-
-// Speaking & Writing Video Page
-app.get("/speaking-writing", (req, res) => res.render("speaking-writing"));
-
-// Dynamic port for Render
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
